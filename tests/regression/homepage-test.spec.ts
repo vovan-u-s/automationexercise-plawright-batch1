b4ec3a22ef2b96de8e238c5a0b7e9e203b588294
vovan-u-s <oversoul6661@gmail.com>
2025-09-29 21:00:29 -0500

    5

--- tests/regression/homepage-test.spec.ts
+++ tests/regression/homepage-test.spec.ts
@@ -1,11 +1,11 @@
 import { test } from '@playwright/test'
-import { HomePage } from '../pages/HomePage';
-import { TestCasePage } from '../pages/testCasePage';
-import { BasePage } from '../pages/BasePage';
+import { HomePage } from '../../pages/HomePage';
+import { TestCasePage } from '../../pages/testCasePage';
+import { BasePage } from '../../pages/BasePage';
 test.describe('Homepage Test', () => {
     test('Verify that home page is visible successfully', async ({ page }) => {
         await page.goto('https://automationexercise.com/');
-        const homePage = new HomePage(page) ;
+        const homePage = new HomePage(page);
         let basePage = new BasePage(page);
         await basePage.clickOnTopNavigationLink('Test Cases');
         await homePage.isLogoVisible();
