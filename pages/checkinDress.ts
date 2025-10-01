2fc9c203f57f6cd6cef3d831b9f334ebd6ca060f
vovan-u-s <oversoul6661@gmail.com>
2025-09-30 20:13:01 -0500

    verified test case 8

---
+++ pages/checkinDress.ts
@@ -0,0 +1,27 @@
+import{test,Page,Locator } from '@playwright/test'
+import { BasePage } from './BasePage';
+export class CheckinDress extends BasePage{
+    name: Locator;
+    category: Locator;
+    price: Locator;
+    availability: Locator;
+    condition: Locator;
+    brand: Locator;
+    constructor(page: Page) {
+        super(page);
+        this.name = page.getByRole('heading', { name: 'Sleeveless Dress' })
+        this.category = page.getByText('Category: Women > Dress')
+        this.price = page.getByText('Rs.')
+        this.availability = page.getByText('Availability: In Stock')
+        this.condition = page.getByText('Condition: New')
+        this.brand = page.getByText('Brand: Madame')
+    }
+    async validateDressDetails(): Promise<void> {
+        await this.name.isVisible();
+        await this.category.isVisible();
+        await this.price.isVisible();
+        await this.availability.isVisible();
+        await this.condition.isVisible();
+        await this.brand.isVisible();
+    }
+}
--- pages/productPage.ts
+++ pages/productPage.ts
@@ -1,4 +1,4 @@
-import { Page, Locator ,expect} from '@playwright/test'
+import { Page, Locator, expect } from '@playwright/test'
 
 
 
@@ -7,24 +7,35 @@ export class ProductPage {
     searchButton: Locator;
     allProductsTitle: Locator;
     allSearchedProducts: Locator;
+    viewProductButton: Locator;
     constructor(page: Page) {
         this.searchBar = page.getByRole('textbox', { name: 'Search Product' })
         this.searchButton = page.getByRole('button', { name: '' })
         this.allProductsTitle = page.getByRole('heading', { name: 'All Products' })
         this.allSearchedProducts = page.locator('div[class="features_items"] div[class="single-products"]');
+        this.viewProductButton = page.locator('.nav.nav-pills.nav-justified > li > a').first()
     }
     async isAllProductsTitleVisible(): Promise<void> {
         await this.allProductsTitle.isVisible();
     }
+    async searchProduct(productName: string): Promise<void> {
+        await this.searchBar.fill(productName);
+        await this.searchButton.click();
+    }
 
     async validateAllSearchedProducts(): Promise<void> {
-        const count=await this.allSearchedProducts.count();
+        const count = await this.allSearchedProducts.count();
         expect(count).toBeGreaterThan(0);
-       for(let i=0;i<await this.allSearchedProducts.count();i++){
-        await expect(this.allSearchedProducts.nth(i)).toBeVisible();
-        console.log(count)
-       }
-        
+        for (let i = 0; i < await this.allSearchedProducts.count(); i++) {
+            await expect(this.allSearchedProducts.nth(i)).toBeVisible();
+            console.log(count)
+        }
     }
+
+    async clickOnFirstProduct(): Promise<void> {
+        await this.viewProductButton.click();
+    }
+
+
 
 }
